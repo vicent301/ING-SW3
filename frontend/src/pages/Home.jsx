d05c97e030c03df73bf1211735f0e184d2e2f509
vicent301 <vicenmonzo38@gmail.com>
2025-12-02 20:08:25 -0300

    cambio de color en titulo y comentario para el final

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -25,7 +25,7 @@ export default function Home() {
 
       {/* 🔹 Contenido visible */}
       <div className="relative z-10 max-w-3xl mx-auto px-6 text-center"
-     style={{ color: "rgb(230,230,230)" }}> 
+     style={{ color: "rgb(230,230,60)" }}> 
 
         <h1
           className="text-6xl md:text-7xl font-bold mb-6 drop-shadow-lg"
@@ -35,6 +35,7 @@ export default function Home() {
           }}
         >
           Bienvenido a ZapaStore 
+          2.0
         </h1>
 
         <p
